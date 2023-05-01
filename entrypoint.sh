b567d3d80f7cc0d9ccf158348ed3a62f582376dc
keena <keynyaan@gmail.com>
2023-05-01 13:51:34 +0900

    feat: db:migrateされるように対応 #49

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ rm -f /backend/tmp/pids/server.pid
 if [ "$RAILS_ENV" = "production" ]; then
   # 本番環境（AWS ECS）への初回デプロイ時に利用
   # 初回デプロイ後にコメントアウトする
-  bundle exec rails db:create
+   # bundle exec rails db:create
   # --------------------------------------
   # 2回目以降のデプロイ時に利用
   # 初回デプロイ後にコメントアウトを外す
-  # bundle exec rails db:migrate
+  bundle exec rails db:migrate
 fi
 
 # サーバー実行(DockerfileのCMDをセット)
