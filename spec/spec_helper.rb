595c444f3984eb19e60b5911a3a04206d56d68da
keena <keynyaan@gmail.com>
2023-08-23 18:04:17 +0900

    refactor:開発環境では、codecovのファイルを出力しないように修正 #125

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 require 'codecov'
+require 'simplecov-cobertura'
 
 SimpleCov.start 'rails' do
   formatter SimpleCov::Formatter::CoberturaFormatter
